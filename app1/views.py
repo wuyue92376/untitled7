ecb118e4d22eaad712b2eeb1192c887ae45f143c
wuyue92376 <603882437@qq.com>
2018-02-09 15:32:49 +0800

    gateone done

--- app1/views.py
+++ app1/views.py
@@ -86,9 +86,71 @@ def console(request):
         result= sshconnet.connectssh(hostname='bogon', port='22', username='root', password='Ji@ye2016', command=command1)
         return render(request,'consloe.html',{'i':result})
 def host_connect(request,nid):
+    import time, hmac, hashlib, json
+    user =nid
+    print(user)
+    gateone_server = "https://172.1.0.102"
+    secret ="ODUwMTk5ZGNhNmYwNGViZTk0MGNlNTk3N2Y5YmY4ODM5Z"
+    api_key ="YWZjY2ZlMjZkM2U4NGQ0ZGJkZDllMzUxYTMzOGFlZWZkZ"
+    authobj = {
+        'api_key': api_key,
+         'upn': user,
+        'timestamp': str(int(time.time() * 1000)),
+        'signature_method': 'HMAC-SHA1',
+        'api_version': '1.0'
+    }
+    my_hash = hmac.new(bytes(secret,'utf-8'),digestmod=hashlib.sha1)
+
+    my_hash.update((authobj['api_key'] + authobj['upn'] + authobj['timestamp']).encode('utf-8'))
+
+    authobj['signature'] = my_hash.hexdigest()
+    print(authobj)
+    # auth_info_and_server = {"url": gateone_server, "auth": authobj}
+    # valid_json_auth_info = json.dumps(auth_info_and_server)
+    valid_json_auth_info = json.dumps(authobj)
     if request.method=='GET':
         v = models.HostInfo.objects.filter(id=nid).values('id', 'HostIP', 'Hostname', 'version', 'app__proline','admin','passwd')
+        return render(request,'shell.html',{'item':v,'auth_info':valid_json_auth_info})
 
 
+# def generate_gate_one_auth_obj(request):
+#     import time, hmac, hashlib, json
+#
+#     user = request.user.username
+#
+#     gateone_server = GATEONE_SERVER // 替换成对应的部署gateone的server的ip地址
+#
+#     secret = GATEONE_API_SECRET // 替换成对应的api
+#     secret ，该信息
+#     存放在gateone的配置文件30api.conf中
+#
+#     api_key = GATEONE_API_KEY // 替换成对应的api
+#     key ，该信息
+#     存放在gateone的配置文件30api.conf中
+#
+#     authobj = {
+#
+#         'api_key': api_key,
+#
+#         'upn': user,
+#
+#         'timestamp': str(int(time.time() * 1000)),
+#
+#         'signature_method': 'HMAC-SHA1',
+#
+#         'api_version': '1.0'
+#
+#     }
+#
+#     my_hash = hmac.new(secret, digestmod=hashlib.sha1)
+#
+#     my_hash.update(authobj['api_key'] + authobj['upn'] + authobj['timestamp'])
+#
+#     authobj['signature'] = my_hash.hexdigest()
+#
+#     auth_info_and_server = {"url": gateone_server, "auth": authobj}
+#
+#     valid_json_auth_info = json.dumps(auth_info_and_server)
+#
+#     # return HttpResponse(valid_json_auth_info)
 
-        return render(request,'shell.html',{'item':v})
